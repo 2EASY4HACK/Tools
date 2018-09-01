180a47223ab788cac23fc1e5fad1499aaf5d3900
2E4H <42806798+2EASY4HACK@users.noreply.github.com>
2018-09-02 00:54:50 +0700

    Add files via upload

---
+++ 2e4h.bash
@@ -0,0 +1,111 @@
+#!/bin/bash
+#InYourHeart Was Here:)
+echo "#================2EASY4HACK================#
+#   _|_|     _|_|_|_|   _|  _|     _|    _|#
+# _|    _|   _|         _|  _|     _|    _|#
+#     _|     _|_|_|     _|_|_|_|   _|_|_|_|#
+#   _|       _|             _|     _|    _|#
+# _|_|_|_|   _|_|_|_|       _|     _|    _|#
+#================InYourHeart===============#
+~~~~~~~~~~######~~~~~~~~~~
+| Author : InYourHeart   |
+| Versi  : 1.0           |
+~~~~~~~~~~######~~~~~~~~~~
+Tool Termux!!
+To Easy For Hacktivias:v
+-_-_-_-_-_-_-_-_-_-_-_-_-_
+~~~~~~Silahkan Pilih~~~~~~
+------MetasploitUser------
+1.  Install Metasploit
+2.  Install Tmux-Bunch | Untuk Inject Backdoor
+3.  Install Ngrok
+4.  Install Shodan     | Intinya Untuk Mengexploit:v
+------**************------
+------PhishingTools-------
+5.  Install Weeman     | Membuat Phising Sendiri
+6.  Install Blackeye   | Memakai Phising Instant
+7.  Install SocialFish
+------*************-------
+-------SosmedTools--------
+8.  Install Hydra      | Membuat BruteForce Sendiri
+9.  Install Cupp       | Untuk Buat Password list
+10. Install MBF        | Hack Fb Metode MultiBruteForce
+11. Install Instahack  | Hack Instagram Dengan Password List
+12. Install Twiter Sniper
+13. Install b3v3r      | Hack Akun Gmail
+-------***********--------
+---------UsersBOT---------
+14. Install Instabot
+15. Install Bot Komen Fb
+16. Install Bot Like Fb
+17. Install Chuckyfix
+---------********---------
+---------Defacers---------
+18. Install Sqlmap
+19. Install LFISuite
+20. Install Bingoo
+21. Install LiteScript
+22. Install XAttacker
+23. Install Diejoubu
+----------Others----------
+24. Install Java
+25. Install Fedora
+26. Install Kali Nethunter
+27. Install Debian
+28. Install Ubuntu
+29. Install Kali Linux
+******Special Tools*******
+30. Install Blackarch
+----------######----------" | lolcat 
+read -p "root@2E4H #:" | lolcat $gg;
+
+if [ $gg = 1 ] || [ $gg = 1 ]
+then
+clear
+pkg update -y
+pkg install wget -y
+wget https://Auxilus.github.io/metasploit.sh
+./metasploit.sh
+cd $HOME/metasploit-framework
+cd /data/data/com.termux/files/home && gem install bundle --pre
+cd /data/data/com.termux/files/home && bundle config build.nokogiri --use-system-libraries
+cd /data/data/com.termux/files/home && bundle install
+echo "**********************************" | lolcat
+echo "Metasploit Telah Sukses Terinstall" | lolcat
+echo "**********************************" | lolcat
+sleep 3
+cd $HOME && ./msfconsole
+fi
+
+if [ $gg =2 ] || [ $gg = 2 ]
+then
+clear
+pkg update -y
+pkg install axel -y
+axel https://github.com/Hax4us/Tmux-Bunch/releases/download/2.4/Tmux-Bunch-2.4.tar.gz
+tar -xf Tmux-Bunch-2.4.tar.gz
+rm -f Tmux-Bunch-2.4.tar.gz
+cd Tmux-Bunch && bash setup
+echo "**********************************" | lolcat
+echo "Tmux-Bunch Telah Sukses Terinstall" | lolcat
+echo "**********************************" | lolcat
+sleep 3
+cd $HOME && tmuxbunch
+fi
+
+if [ $gg =3 ] || [ $gg = 3 ]
+then
+clear
+pkg update -y
+pkg install wget -y
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
+unzip ngrok-stable-linux-arm.zip
+rm -f ngrok-stable-linux-arm.zip
+echo "*****************************" | lolcat
+echo "Ngrok Telah Sukses Terinstall" | lolcat
+echo "*****************************" | lolcat
+sleep 3
+fi
+
+
+
