2870bd15ddcdf577bff8fe603f00b3ee63abb6ed
2E4H <42806798+2EASY4HACK@users.noreply.github.com>
2018-09-17 01:15:37 +0700

    Add files via upload

---
+++ N1CKN4M3.sh
@@ -0,0 +1,367 @@
+#!/data/data/com.termux/files/usr/bin/bash
+#ngapain TOD LU MAU RECODE YA??
+#GAK BAKALAN JADI MASTAH LU NJIRR
+#GANTI AUTHOR DOANG HAHAHA AUTO BULLY
+clear
+#colors
+blue='\e[1;34m'
+green='\e[1;32m'
+purple='\e[1;35m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+white='\e[1;37m'
+yellow='\e[1;33m'
+lightgreen='\e[1;32m'
+NOW=`date "+%d.%m.%Y"`
+TIME=`date "+%H:%M"`
+cowsay -f eyes "PILIH DENGAN BIJAK" |lolcat
+figlet -f slant "TU4N N1CKN4M3" | lolcat
+echo  ●▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ஜஜஜஜஜஜஜஜஜஜஜஜஜஜஜஜஜஜ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬$
+echo  " AUTHOR      : TU4N N1CKN4M3  " |lolcat
+echo  " ALAMAT      : ADA DI KULKAS"|lolcat
+echo  " THANKS TO   : 2E4SY4HACK      "|lolcat
+echo  " THANKS TO   : Mr.Jackers" |lolcat
+echo  " THANKS TO   : PANDEGLANG CYBER TEAM"|lolcat
+echo  " THANKS TO   : BANTEN CYBER TEAM"
+echo  " THANKS TO   : INDONESIAN TERMUX ASSOCIATION"|lolcat
+echo  " Date        : " $NOW | lolcat
+echo  " Time        : " $TIME | lolcat
+cowsay -f slant "NUUB STAH" |lolcat
+echo "+×+××+×+×+×+×+×++×+×+×××+××+×+××+×++×+×+×+×"|lolcat
+echo "+                                         ×"|lolcat
+echo "x            2EASY4HACKTIVIS              +"|lolcat
+echo "+                                         ×"|lolcat
+echo "+                                         ×"|lolcat
+echo "+          WE PARTY INYOUR SYSTEM         +"|lolcat
+echo "+                                         ×"|lolcat
+echo "+                                         ×"|lolcat
+echo "+×+××+×+×+×+×+×++×+×+×××+××+×+××+×++×+×+×+×"|lolcat
+
+
+echo "TOOLS NEWBIE JANGAN DI BULLY ^_^"|lolcat
+echo "sekecil apapun karyamu akan tetap indah jika kau"|lolcat
+echo "nikmati dan jauhi dari cemooh orang lain"|lolcat
+
+echo "÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷"|lolcat
+
+echo "          PILIH SESUAI KEBUTUHAN :v"
+
+echo "÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷"|lolcat
+
+
+
+echo "[+]=========================================[+]"|lolcat
+echo "[+]           1.METASPLOIT                  [+]"|lolcat
+echo "[+]=========================================[+]"|lolcat
+echo "[+]=========================================[+]"|lolcat
+echo "[+]           2.HACK FB MASAL TOD           [+]"|lolcat
+echo "[+]=========================================[+]"|lolcat
+echo "[+]=========================================[+]"|lolcat
+echo "[+]           3.SQLMAP BUAT DUMP            [+]"|lolcat
+echo "[+]=========================================[+]"|lolcat
+echo "[+]=========================================[+]"|lolcat
+echo "[+]           4.KO-DORK BUAT CARI WEB VULN  [+]"|lolcat
+echo "[+]=========================================[+]"|lolcat
+echo "[+]           5.AOC DEFACE                  [+]"|lolcat
+echo "[+]=========================================[+]"|lolcat
+echo "[+]=========================================[+]"|lolcat
+echo "[+]           6.Black Hydra                 [+]"|lolcat
+echo "[+]=========================================[+]"|lolcat
+echo "[+]           7.WEEMAN BUAT PHISING         [+]"|lolcat
+echo "[+]=========================================[+]"|lolcat
+echo "[+]           8.TOOLS TU4N B4DUT            [+]"|lolcat
+echo "[+]=========================================[+]"|lolcat
+echo "[+]           9.LOGIN ADMIN FINDER          [+]"|lolcat
+echo "[+]=========================================[+]"|lolcat
+echo "[+]           10.B4J1N64Nv4                 [+]"|lolcat
+echo "[+]=========================================[+]"|lolcat
+echo "[+]           11.BOBOL WIFI ROOT CUK        [+]"|lolcat
+echo "[+]=========================================[+]"|lolcat
+echo "[+]           12.INSTALL SPAM SMS           [+]"|lolcat
+echo "[+]=========================================[+]"|lolcat
+
+echo       "+++++++++++++++++++++"|lolcat
+echo       "+    99.EXIT SOB     +"|lolcat
+echo       "+++++++++++++++++++++"|lolcat
+echo "udah segitu aja dlu yak tod"|lolcat
+trap ctrl_c INT
+ctrl_c()
+{
+echo -e "$red(Ctrl + C ) segini dulu ya tod capek gua "
+figlet -f digital "     BYEEE"
+sleep 1
+exit
+}
+read -p "Silahkan Pilih :" gg
+
+if [ $gg = 1 ] || [ $gg = 1 ]
+then
+clear
+cd /data/data/com.termux/files/home
+pkg update -y
+pkg install wget -y
+wget https://Auxilus.github.io/metasploit.sh
+./metasploit.sh
+cd $HOME/metasploit-framework
+cd /data/data/com.termux/files/home && gem install bundle --pre
+cd /data/data/com.termux/files/home &&
+bundle config build.nokogiri --use-system-libraries
+cd /data/data/com.termux/files/home && bundle install
+echo "Metasploit udah tod ^_^" | lolcat
+sleep 1
+sleep 1
+cd $HOME && ./msfconsole
+fi
+if [ $gg = 1 ] || [ $gg = 1 ]
+then
+cd $HOME
+fi
+if [ $gg = 2 ] || [ $gg = 2 ]
+then
+clear
+cd $HOME
+pkg update -y
+pkg install python2  -y
+pkg install git -y
+pip2 install mechanize
+git clone https://github.com/tikuskecil/multi-bruteforce-facebook
+cd multi-bruteforce-facebook
+echo "GASS MBF DAH SIAP" | lolcat
+sleep 1
+python2 MBF.py
+fi
+
+if
+[ $gg = 2 ] || [ $gg = 2 ]
+then
+cd $HOME
+fi
+
+if
+ [ $gg = 3 ] || [ $gg = 3 ]
+then
+clear
+cd $HOME
+pkg update -y
+pkg install git -y
+pkg install python2 -y
+git clone https://github.com/sqlmapproject/sqlmap.git
+cd sqlmap
+echo "gunakan -u (masukin target) --dbs (nyari database) --T (cari table)"|lolcat
+sleep 1
+python2 sqlmap.py
+fi
+
+if
+ [ $gg = 3 ] || [ $gg = 3 ]
+then
+clear
+cd $HOME
+fi
+
+if
+ [ $gg = 4 ] || [ $gg = 4 ]
+then
+clear
+cd $HOME
+apt upgrade -y
+apt install git -y
+apt install python2 -y
+git clone https://github.com/CiKu370/ko-dork
+cd ko-dork
+sleep 1
+python2 dork.py
+fi
+
+if
+ [ $gg = 4 ] || [ $gg = 4 ]
+then
+clear
+cd $HOME
+fi
+
+if
+ [ $gg = 5 ] || [ $gg = 5 ]
+then
+clear
+cd $HOME
+apt update -y
+apt upgrade -y
+apt install git -y
+git clone https://github.com/Amriez/AOCDEFACE.git
+cd AOCDEFACE
+sh install.sh
+sleep 1
+sh AOC.sh
+fi
+
+if
+ [ $gg = 5 ] || [ $gg = 5 ]
+then
+clear
+cd $HOME
+fi
+
+if
+ [ $gg = 6 ] || [ $gg = 6 ]
+then
+clear
+cd $HOME
+pkg update -y
+pkg install git hydra -y
+pkg install git -y
+pkg install python -y
+pkg install python2 -y
+git clone https://github.com/Gameye98/Black-Hydra
+cd Black-Hydra
+chmod +x blackhydra.py
+sleep 1
+python2 blackhydra.py
+fi
+
+if
+ [ $gg = 6 ] || [ $gg = 6 ]
+then
+clear
+cd $HOME
+fi
+
+if [ $gg = 7 ] || [ $gg = 7 ]
+then
+clear
+cd $HOME
+pkg update -y
+pkg upgrade -y
+pkg install python -y
+pkg install python2 -y
+pkg install git -y
+git clone https://github.com/samyoyo/weeman
+chmod +x weeman
+cd weeman
+sleep 1
+python2 weeman.py
+fi
+
+if [ $gg = 7 ] || [ $gg = 7 ]
+then
+clear
+cd $HOME
+fi
+
+if [ $gg = 8 ] || [ $gg = 8 ]
+then
+clear
+cd $HOME
+pkg update -y
+pkg upgrade -y
+pkg install python python2 vim figlet curl
+pkg install php
+pip2 install lolcat
+pkg install git
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv4
+chmod 777 TUANB4DUT..sh
+sleep1
+./TUANB4DUT..sh
+fi
+
+if [ $gg = 8 ] || [ $gg = 8 ]
+then
+clear
+cd $HOME
+fi
+
+if [ $gg = 9 ] || [ $gg = 9 ]
+then
+clear
+cd $HOME
+apt update && apt upgrade -y
+apt install python2 git
+git clone https://github.com/Techzindia/admin_penal/adminpanel/
+cd adminpanel
+mv admin_panel_finder.py main.py
+chmod +x main.py
+sleep1
+python2 main.py
+fi
+
+if [ $gg = 9 ] || [ $gg = 9 ]
+then
+clear
+cd $HOME
+fi
+
+if [ $gg = 10 ] || [ $gg = 10 ]
+then
+clear
+cd $HOME
+pkg install git php python2 cowsay figlet toilet ruby -y
+pkg install lolcat -y
+gem install lolcat
+pip2 install toilet
+pip2 install lolcat
+git clone https://github.com/DarknessCyberTeam/B4J1N64Nv4
+cd B4J1N64Nv4
+sleep 1
+sh bajingan.sh
+fi
+
+if [ $gg = 10 ] || [ $gg = 10 ]
+then
+clear
+cd $HOME
+fi
+
+if [ $gg = 11 ] || [ $gg = 11 ]
+then
+clear
+cd $HOME
+apt update -y
+pkg install git -y
+git clone https://github.com/esc0rtd3w/wifi-hacker
+cd wifi-hacker
+chmod +x wifi-hacker.sh
+sleep1
+./wifi-hacker.sh
+fi
+
+if [ $gg = 11 ] || [ $gg = 11 ]
+then
+clear
+cd $HOME
+fi
+
+if [ $gg = 12 ] || [ $gg = 12 ]
+then
+clear
+cd $HOME
+apt update && apt upgrade -y
+apt install php -y
+apt install python2 -y
+apt install toilet -y
+apt install git -y
+git clone https://github.com/4L13199/LITESPAM
+cd LITESPAM
+sleep 1
+sh LITESPAM.sh
+fi
+
+if [ $gg = 12 ] || [ $gg = 12 ]
+then
+clear
+cd $HOME
+fi
+
+if [ $gg = 99 ] || [ $gg = 99 ]
+then
+echo " IM NOT HACKER"
+sleep 1
+echo "I'M USER""
+sleep 1
+echo " 2E4H"|lolcat
+sleep 1
+echo "We Party InYour System"|lolccat
+sleep 1
+echo "TU4N N1CKN4M3"|lolcat
+sleep 1
+exit
+fi
